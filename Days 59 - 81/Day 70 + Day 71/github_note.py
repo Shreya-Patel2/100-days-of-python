187e4f1db5be10967d5ef9d48949aee592126b56
Shreya Patel <shreyap2308@gmail.com>
2025-09-29 18:11:29 +0100

    Add files via upload

---
+++ Days 59 - 81/Day 70 + Day 71/github_note.py
@@ -0,0 +1,11 @@
+# Day 70 + Day 71 consisted of a series of introductory GitHub videos.
+# Topics covered include:
+# --- Using the Terminal
+# --- Version Control
+# --- Remote Repositories
+# --- Gitignore
+# --- Cloning
+# --- Branching
+# --- Merging
+# --- Forking
+# --- Pull Requests
